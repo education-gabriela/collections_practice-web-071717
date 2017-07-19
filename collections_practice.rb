bcad33adc442f358701abfed00b98953496191ac
gabidavila <gabidavila@gmail.com>
2017-07-18 21:59:44 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  index_placeholder_value = array[index]
+  destination_placeholder_index = array[destination_index]
+
+  array[destination_index] = index_placeholder_value
+  array[index] = destination_placeholder_index
+
+  array
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1, 2)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |item|
+    item[2] = '$'
+    item
+  end
+end
+
+def find_a(array)
+  array.select do |item|
+    item.downcase.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.collect.with_index do |item, index|
+    index != 1 ? item + "s" : item
+  end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  describe '#swap_elements_from_to' do
+    it 'swaps between two any given positions of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 0, 2)).to eq(["scott", "ashley", "blake"])
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 2, 1)).to eq(["blake", "scott", "ashley"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
